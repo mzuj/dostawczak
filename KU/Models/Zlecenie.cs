2bdad2bf15c06db9bd293d849f3b9201b204daba
Mzuj <your_email@youremail.com>
2014-04-13 14:51:59 +0200

    Nowdy model

--- KU/Models/Zlecenie.cs
+++ KU/Models/Zlecenie.cs
@@ -19,17 +19,25 @@ namespace KU.Models
         public string Miejsce_dostawy { get; set; }
         public string Odbiorca { get; set; }
         public string Zleceniodawca { get; set; }
-        public string Zawartosc { get; set; }
         public Nullable<int> Ilosc_opakowan { get; set; }
-        public string Rodzaj_opakowan { get; set; }
         public bool Materialy_niebezpieczne { get; set; }
         public bool Pobranie_za_przesylke { get; set; }
         public bool Priorytet { get; set; }
         public string Kategoria_zlecenia { get; set; }
         public string Kurier { get; set; }
         public int Status { get; set; }
+        public string Komentarz_kuriera { get; set; }
+        public string Komentarz_nadawcy { get; set; }
+        public int RodzajOpakowaniaId { get; set; }
+        public int ZawartoscId { get; set; }
+        public Nullable<int> PowodOdrzuceniaId { get; set; }
+        public Nullable<int> PowodPrzelozeniaId { get; set; }
     
         public virtual AspNetUsers AspNetUsers { get; set; }
         public virtual StatusZlecenie StatusZlecenie { get; set; }
+        public virtual RodzajOpakowania RodzajOpakowania { get; set; }
+        public virtual Zawartosc Zawartosc { get; set; }
+        public virtual PowodOdrzucenia PowodOdrzucenia { get; set; }
+        public virtual PowodPrzelozenia PowodPrzelozenia { get; set; }
     }
 }
