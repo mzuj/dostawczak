32af0c9719a7d2949957e8d3bd49d1d3a7957aaf
Mzuj <your_email@youremail.com>
2014-04-13 12:36:11 +0200

    Ustawianie statusów zleceń

--- KU/Logic/ErrandStatusHelper.cs
+++ KU/Logic/ErrandStatusHelper.cs
@@ -3,19 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using KU.Models;
+using System.Data.Entity;
 
 namespace KU.Logic
 {
-    public class StatusIdHelper 
+    public class ErrandStatusHelper 
     {
         private ZlecenieEntities db = new ZlecenieEntities();
 
-        public int getStatusIdByName(String statusName) 
+        public int GetStatusIdByName(String statusName) 
         {
             var idStatusCompleted = from s in db.StatusZlecenie
                                     where s.Nazwa.Equals(statusName)
                                     select s.Id;
             return idStatusCompleted.First();
+        }
+
+        public void SetErrandStatus(String statusName, int erandId)
+        {
+            var statusIdToSet = GetStatusIdByName(statusName);
+            var errandToSetStatus = db.Zlecenie.Find(erandId);
+
+            errandToSetStatus.Status = statusIdToSet;
+            db.SaveChanges();
         }
     }
 }
